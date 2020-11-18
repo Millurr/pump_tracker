08a84b1df6aaeead42e9e8e3c6a3f7b3a4c03cfa
Millurr <42452986+Millurr@users.noreply.github.com>
2020-11-18 09:42:37 -0600

    delayed updates to chart screen

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -2,8 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'dart:math';
 
 class ChartScreen extends StatefulWidget {
   final Widget child;
@@ -15,36 +13,16 @@ class ChartScreen extends StatefulWidget {
 
 class _ChartScreenState extends State<ChartScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  List<charts.Series<Sales, int>> _seriesLineData;
+  List<charts.Series<Sales, int>> _seriesLineData =
+      new List<charts.Series<Sales, int>>();
   List<String> dropwDownList = new List<String>();
   String _currentName;
-
-  _generateData() {
-    var linesalesdata = [
-      new Sales(0, 45),
-      new Sales(1, 56),
-      new Sales(2, 55),
-      new Sales(3, 60),
-      new Sales(4, 61),
-      new Sales(5, 70),
-    ];
-
-    _seriesLineData.add(
-      charts.Series(
-        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xff990099)),
-        id: 'Air Pollution',
-        data: linesalesdata,
-        domainFn: (Sales sales, _) => sales.yearval,
-        measureFn: (Sales sales, _) => sales.salesval,
-      ),
-    );
-  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _seriesLineData = List<charts.Series<Sales, int>>();
+    // _seriesLineData = List<charts.Series<Sales, int>>();
     // _generateData();
   }
 
